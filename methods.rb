94041790103850e0ecb05c893eeeb2f59d623b83
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-10-15 09:53:36 +0300

    completed ruby methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,45 @@
 # Your code here!
+def greet_programmer ()
+    puts "Hello, programmer!" 
+end
+
+
+def greet ( name = "Naureen ")
+    puts "Hello, #{name}!"
+end
+
+
+def greet ( name = "Jimmy ")
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+  end
+
+
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+
+def add (num1, num2 )
+    return num1 + num2
+    end
+
+
+def halve_number ( number)
+    return number / 2
+end
+    if number.class != Integer
+        return nil
+    end
+
+    # def reverse_name(name)
+    #     if name.class != String
+    #       return nil
+    #     end
+      
+    #     name.reverse
+    #   end
